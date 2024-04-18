0080a5ed5890c13d46ac6a621b06a2d8a35c8e2b
dhaillant <david@davidhaillant.com>
2024-04-18 13:07:53 +0200

    license

--- rec/rec.ino
+++ rec/rec.ino
@@ -7,10 +7,32 @@
  * REC is a record-and-play Eurorack module
  * 
  * Copyright 2024 David Haillant
+ * www.davidhaillant.com
+ * 
  * 
  * Compatible hardware:
  * REC V0.1 (20240315)
  * 
+ */
+
+/* 
+ * This program is free software: you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <https://www.gnu.org/licenses/>. 
+ */
+
+
+
+/* 
  * 
  * How does it work?
  * =================
@@ -31,7 +53,7 @@
  * 
  * While recording, the CV output will reproduce immediately the recorded sample
  * 
-*/
+ */
 
 
 #define DEBUG
